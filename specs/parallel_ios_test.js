2c379ae181c2af59147d6c4d5ddbfbfd981be843
AayushiSaini <as8472161@gmail.com>
2026-01-27 00:49:40 +0530

    Final fix: Added forBrowser to the correct specs file

--- specs/parallel_ios_test.js
+++ specs/parallel_ios_test.js
@@ -1,18 +1,17 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
-const assert = require("assert");
 const conf = require("../conf/parallel_ios.conf.js");
 
 const LT_USERNAME = process.env.LT_USERNAME || conf.LT_USERNAME;
 const LT_ACCESS_KEY = process.env.LT_ACCESS_KEY || conf.LT_ACCESS_KEY;
-
 const gridUrl = "hub.lambdatest.com/wd/hub";
 
 describe("LambdaTest iOS Parallel Test", function () {
   this.timeout(300000);
-  let driver;
 
   conf.capabilities.forEach(function (caps) {
     it("should run test on " + caps["appium:deviceName"], async function () {
+      
+      // Capabilities merge kar rahe hain LambdaTest credentials ke sath
       const capabilities = {
         ...caps,
         "lt:options": {
@@ -22,15 +21,21 @@ describe("LambdaTest iOS Parallel Test", function () {
         },
       };
 
-      driver = await new Builder()
+      // YAHAN HAI FIX: .forBrowser('safari') add kiya hai
+      let driver = await new Builder()
         .usingServer("https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + "@" + gridUrl)
+        .forBrowser('safari') 
         .withCapabilities(capabilities)
         .build();
 
       try {
-        // Aapka Test Logic yahan aayega
+        // Simple test: Google open karke title check karna
         await driver.get("https://google.com");
-        console.log("Test Passed for " + caps["appium:deviceName"]);
+        let title = await driver.getTitle();
+        console.log("Browser title for " + caps["appium:deviceName"] + " is: " + title);
+      } catch (err) {
+        console.log("Error on " + caps["appium:deviceName"] + ": " + err.message);
+        throw err;
       } finally {
         await driver.quit();
       }
