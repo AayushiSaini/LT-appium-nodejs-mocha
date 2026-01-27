01ebb2f07f156b3c9131e0dda004c2ee389f623c
AayushiSaini <as8472161@gmail.com>
2026-01-27 11:36:46 +0530

    Enabled loop for multiple android devices

--- specs/parallel_android_test.js
+++ specs/parallel_android_test.js
@@ -4,32 +4,35 @@ const conf = require("../conf/parallel_android.conf.js");
 const LT_USERNAME = process.env.LT_USERNAME || conf.LT_USERNAME;
 const LT_ACCESS_KEY = process.env.LT_ACCESS_KEY || conf.LT_ACCESS_KEY;
 
-describe("LambdaTest Android Automation", function () {
-    this.timeout(300000);
-    let driver;
+// Capabilities array se loop chalayenge parallel execution ke liye
+conf.capabilities.forEach(function (caps) {
+    
+    describe("Android Test on " + caps['appium:deviceName'], function () {
+        this.timeout(300000);
+        let driver;
 
-    before(async function () {
-        const caps = conf.capabilities[0];
-        driver = await new Builder()
-            .usingServer(`https://${LT_USERNAME}:${LT_ACCESS_KEY}@mobile-hub.lambdatest.com/wd/hub`)
-            .withCapabilities(caps)
-            .forBrowser('')
-            .build();
-    });
+        before(async function () {
+            driver = await new Builder()
+                .usingServer(`https://${LT_USERNAME}:${LT_ACCESS_KEY}@mobile-hub.lambdatest.com/wd/hub`)
+                .withCapabilities(caps)
+                .forBrowser('')
+                .build();
+        });
 
-    it('Should interact with Proverbial App', async function () {
-        try {
-            console.log("App Launched Successfully!");
-            let colorBtn = await driver.wait(until.elementLocated(By.id('com.lambdatest.proverbial:id/color')), 30000);
-            await colorBtn.click();
-            console.log("Success: Clicked Color button");
-        } catch (e) {
-            console.error("Test failed: ", e.message);
-            throw e;
-        }
-    });
+        it('Should interact with Proverbial App on ' + caps['appium:deviceName'], async function () {
+            try {
+                console.log("App Launched Successfully on " + caps['appium:deviceName']);
+                let colorBtn = await driver.wait(until.elementLocated(By.id('com.lambdatest.proverbial:id/color')), 30000);
+                await colorBtn.click();
+                console.log("Success: Clicked Color button on " + caps['appium:deviceName']);
+            } catch (e) {
+                console.error("Test failed: ", e.message);
+                throw e;
+            }
+        });
 
-    after(async function () {
-        if (driver) await driver.quit();
+        after(async function () {
+            if (driver) await driver.quit();
+        });
     });
 });
